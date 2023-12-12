0cf73e118a41402354de6ebe8faef7b6b548ee64
Merch111 <152105260+Merch111@users.noreply.github.com>
2023-12-12 10:29:17 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
 class Main {
   public static void main(String[] args) {
+    String parolaChiave;
+    System.out.println("Inserisci la parola chiave: ");
+    try{
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+    parolaChiave = keyboard.readLine();
+    } catch(IOException) {
+      System.err-println("Errore nell'inserimento dati");
+    }
+    
+    
     System.out.println("Cifriamo il nostro testo e trasformiamolo in un testo segreto!");
+
     
   Matrice m = new Matrice ("TPSIT");
   Vigenere v = new Vigenere(0,12,0,12,m);
